9c396e039ca2844cbcc6a4f8aa4c607468273261
vinsent <vintrs@163.com>
2018-04-20 16:45:16 +0800

    add UILabel extension function

--- ZZUtils/ZZExtension/UILabel+ZZExtension.h
+++ ZZUtils/ZZExtension/UILabel+ZZExtension.h
@@ -10,4 +10,40 @@
 
 @interface UILabel (ZZExtension)
 
+
+/**
+ fontSize textColor
+ */
++ (UILabel *)labelWithFont:(CGFloat)fontSize textColor:(UIColor *)color superView:(UIView *)superView;
+
+
+/**
+ fontSize textColor backgroundColor
+ */
++ (UILabel *)labelWithFont: (CGFloat)fontSize textColor: (UIColor *)color backgroundColor: (UIColor *)backgroundColor superView: (UIView *)superView;
+
+
+/**
+ fontSize textColor backgroundColor cornerRadious
+ */
++ (UILabel *)labelWithFont: (CGFloat)fontSize textColor: (UIColor *)color backgroundColor: (UIColor *)backgroundColor cornerradious: (CGFloat)corner superView: (UIView *)superView;
+
+
+/**
+ title fontSize textColor
+ */
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color superView:(UIView *)superView;
+
+
+/**
+ title fontSize textColor backgroundColor
+ */
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor superView:(UIView *)superView;
+
+
+/**
+ title font textColor backgroundColor cornerRadious
+ */
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor cornerradious:(CGFloat)corner superView:(UIView *)superView;
+
 @end
--- ZZUtils/ZZExtension/UILabel+ZZExtension.m
+++ ZZUtils/ZZExtension/UILabel+ZZExtension.m
@@ -10,4 +10,44 @@
 
 @implementation UILabel (ZZExtension)
 
++ (UILabel *)labelWithFont:(CGFloat)fontSize textColor:(UIColor *)color superView:(UIView *)superView {
+  UILabel *label = [[UILabel alloc] init];
+  label.font = [UIFont systemFontOfSize:fontSize];
+  label.textColor = color;
+  [superView addSubview:label];
+  return label;
+}
+
++ (UILabel *)labelWithFont:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor superView:(UIView *)superView {
+  UILabel *label = [self labelWithFont:fontSize textColor:color superView:superView];
+  label.backgroundColor = backgroundColor;
+  return label;
+}
+
++ (UILabel *)labelWithFont:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor cornerradious:(CGFloat)corner superView: (UIView *)superView {
+  UILabel *label = [self labelWithFont:fontSize textColor:color backgroundColor:backgroundColor superView:superView];
+  label.layer.cornerRadius = corner;
+  label.font = [UIFont systemFontOfSize:12];
+  return label;
+}
+
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color superView:(UIView *)superView {
+  UILabel *label = [self labelWithFont:fontSize textColor:color superView:superView];
+  label.text = title;
+  return label;
+}
+
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor superView:(UIView *)superView {
+  UILabel *label = [self labelWithTitle:title font:fontSize textColor:color superView:superView];
+  label.backgroundColor = backgroundColor;
+  return label;
+}
+
++ (UILabel *)labelWithTitle:(NSString *)title font:(CGFloat)fontSize textColor:(UIColor *)color backgroundColor:(UIColor *)backgroundColor cornerradious:(CGFloat)corner superView:(UIView *)superView {
+  UILabel *label = [self labelWithTitle:title font:fontSize textColor:color backgroundColor:backgroundColor superView:superView];
+  label.layer.cornerRadius = corner;
+  return label;
+}
+
+
 @end
