72287d523b7cbf91cee31933a81bf2fe087e5ca4
vinsent <vintrs@163.com>
2018-04-23 16:32:28 +0800

    add some generate function for UIButton

--- ZZUtils/ZZExtension/UIButton+ZZExtension.h
+++ ZZUtils/ZZExtension/UIButton+ZZExtension.h
@@ -12,14 +12,51 @@
 
 
 /**
- button圆角 (设置一张带圆角的纯色图片)
+ title font titleColor backgroundColor
+ */
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor superView:(UIView *)superV;
+
+
+/**
+ title font titleColor backgroundColor corner
+ */
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor cornerRadius:(CGFloat)corner superView:(UIView *)superV;
+
+
+/**
+ corner backgroundColor(normal&select) titleColor(normal&select) font title
+ */
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleNormalColor:(UIColor *)titleCol backgroundNormalColor:(UIColor *)backColor titleSelectColor:(UIColor *)selectTitleColor backSelectColor:(UIColor *)selectBackCol cornerRadius:(CGFloat)corner superView:(UIView *)superV;
+
+
+/**
+ corner borderColor backgroundcolor titlecolor font title
+ */
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor borderColor:(UIColor *)borderCol cornerRadius:(CGFloat)corner superView:(UIView *)superV;
+
+- (void)addTarget:(nullable id)target action:(SEL)action;
+
+
+/**
+ 设置状态的纯色背景
+ */
+- (void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state;
+
+
+/**
+ 设置状态的纯色背景和圆角
+ */
+- (void)setBackgroundColor:(UIColor *)color cornRadius:(CGFloat)cornRadius forState:(UIControlState)state;
+
+//背景图片绘制
+/**
+ 设置纯色背景和圆角
 
  @param color 背景颜色
- @param cornRadius 圆角半径
+ @param cornRadius 圆角大小
  @param isOpaque 是否需要不透明
+ @param state button的状态
  */
-- (void)zz_setBackgroundImageWithColor:(UIColor *)color cornRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque;
-- (void)zz_setCornerRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque;
-- (void)zz_setCornerRadius:(CGFloat)cornRadius;
+- (void)setBackgroundColor:(UIColor *)color cornRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque forState:(UIControlState)state;
 
 @end
--- ZZUtils/ZZExtension/UIButton+ZZExtension.m
+++ ZZUtils/ZZExtension/UIButton+ZZExtension.m
@@ -10,26 +10,77 @@
 
 @implementation UIButton (ZZExtension)
 
-//背景图片绘制
-- (void)zz_setBackgroundImageWithColor:(UIColor *)color cornRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.bounds.size.width, self.bounds.size.height)];
-    view.backgroundColor = color;
-    view.layer.cornerRadius = cornRadius;
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor superView:(UIView *)superV {
+  return [[self alloc] initWithTitle:title textFont:font titleNormalColor:titleCol backgroundNormalColor:backColor titleSelectColor:titleCol backSelectColor:backColor cornerRadius:0 superView:superV];
+}
+
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor cornerRadius:(CGFloat)corner superView:(UIView *)superV {
+  return [[self alloc] initWithTitle:title textFont:font titleNormalColor:titleCol backgroundNormalColor:backColor titleSelectColor:titleCol backSelectColor:backColor cornerRadius:corner superView:superV];
+}
+
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleNormalColor:(UIColor *)titleCol backgroundNormalColor:(UIColor *)backColor titleSelectColor:(UIColor *)selectTitleColor backSelectColor:(UIColor *)selectBackCol cornerRadius:(CGFloat)corner superView:(UIView *)superV {
+  return [[self alloc] initWithTitle:title textFont:font titleNormalColor:titleCol backgroundNormalColor:backColor titleSelectColor:selectTitleColor backSelectColor:selectBackCol cornerRadius:corner superView:superV];
+}
+
+- (instancetype)initWithTitle:(NSString *)title textFont:(CGFloat)font titleNormalColor:(UIColor *)titleCol backgroundNormalColor:(UIColor *)backColor titleSelectColor:(UIColor *)selectTitleColor backSelectColor:(UIColor *)selectBackCol cornerRadius:(CGFloat)corner superView:(UIView *)superV {
+  if (self = [super initWithFrame:CGRectZero]) {
+    [self setTitle:title forState:UIControlStateNormal];
+    self.titleLabel.font = [UIFont systemFontOfSize:font];
+    [self setTitleColor:titleCol forState:UIControlStateNormal];
+    [self setTitleColor:selectTitleColor forState:UIControlStateSelected];
+    [self setBackgroundColor:backColor cornRadius:corner opaque:YES forState:UIControlStateNormal];
+    [self setBackgroundColor:selectBackCol cornRadius:corner opaque:YES forState:UIControlStateSelected];
     
-    //第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数是屏幕密度
-    UIGraphicsBeginImageContextWithOptions(view.bounds.size, isOpaque, [UIScreen mainScreen].scale);
-    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    [self setBackgroundImage:image forState:UIControlStateNormal];
+    [superV addSubview:self];
+  }
+  return self;
 }
 
-- (void)zz_setCornerRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque {
-    [self zz_setBackgroundImageWithColor:self.backgroundColor cornRadius:cornRadius opaque:isOpaque];
+
++ (instancetype)buttonWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor borderColor:(UIColor *)borderCol cornerRadius:(CGFloat)corner superView:(UIView *)superV {
+  return [[self alloc] initWithTitle:title textFont:font titleColor:titleCol backgroundColor:backColor borderColor:borderCol cornerRadius:corner superView:superV];
 }
 
-- (void)zz_setCornerRadius:(CGFloat)cornRadius {
-    [self zz_setCornerRadius:cornRadius opaque:YES];
+- (instancetype)initWithTitle:(NSString *)title textFont:(CGFloat)font titleColor:(UIColor *)titleCol backgroundColor:(UIColor *)backColor borderColor:(UIColor *)borderCol cornerRadius:(CGFloat)corner superView:(UIView *)superV {
+  if (self = [super initWithFrame:CGRectZero]) {
+    [self setTitle:title forState:UIControlStateNormal];
+    self.titleLabel.font = [UIFont systemFontOfSize:font];
+    [self setTitleColor:titleCol forState:UIControlStateNormal];
+    [self setBackgroundColor:backColor cornRadius:corner opaque:YES forState:UIControlStateNormal];
+    
+    self.layer.borderColor = borderCol.CGColor;
+    self.layer.borderWidth = 1.f;
+    
+    [superV addSubview:self];
+  }
+  return self;
+}
+
+- (void)addTarget:(nullable id)target action:(SEL)action {
+  [self addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+}
+
+//背景图片绘制
+- (void)setBackgroundColor:(UIColor *)color forState:(UIControlState)state {
+  [self setBackgroundColor:color cornRadius:0 opaque:YES forState:state];
+}
+
+- (void)setBackgroundColor:(UIColor *)color cornRadius:(CGFloat)cornRadius forState:(UIControlState)state {
+  [self setBackgroundColor:color cornRadius:cornRadius opaque:YES forState:UIControlStateNormal];
+}
+
+//背景图片绘制
+- (void)setBackgroundColor:(UIColor *)color cornRadius:(CGFloat)cornRadius opaque:(BOOL)isOpaque forState:(UIControlState)state {
+  UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.bounds.size.width, self.bounds.size.height)];
+  view.backgroundColor = color;
+  view.layer.cornerRadius = cornRadius;
+  
+  //第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数是屏幕密度
+  UIGraphicsBeginImageContextWithOptions(view.bounds.size, isOpaque, [UIScreen mainScreen].scale);
+  [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  [self setBackgroundImage:image forState:state];
 }
 
 @end
